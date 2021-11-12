44f97a9073dd524983d713390ee7e8c87b4440b5
jamgu <geniusgara@gmail.com>
2021-11-12 12:46:18 +0800

    feat:新增不准时的定时器

---
+++ app/src/main/java/com/jamgu/hwstatistics/timer/Timer.kt
@@ -0,0 +1,44 @@
+package com.jamgu.hwstatistics.timer
+
+import android.os.Handler
+import android.os.Looper
+
+/**
+ * Created by jamgu on 2021/11/11
+ */
+class Timer(private val looper: Looper) {
+
+    private var mHandler: Handler? = null
+
+    private lateinit var mMission: Runnable
+
+    private var isRunning: Boolean = false
+
+    fun isStarted(): Boolean {
+        return isRunning
+    }
+
+    fun run(mission: () -> Unit, delay: Long) {
+        if (isRunning) return
+
+        mHandler = Handler(looper)
+
+        object : Runnable {
+            override fun run() {
+                mission.invoke()
+
+                isRunning = true
+                mHandler?.postDelayed(this, delay)
+            }
+        }.also { mMission = it }
+
+        mHandler?.post(mMission)
+    }
+
+    fun close() {
+        isRunning = false
+        mHandler?.removeCallbacks(mMission)
+        mHandler = null
+    }
+
+}
